e9fe84698e63eec87637c8525d83678ba9faf63d
angelgarcia11@aragon.unam.mx <angelgarcia11@aragon.unam.mx>
2021-03-04 15:09:51 -0600

    subiendo archivos

---
+++ Proyecto2209/src/main/java/mx/unam/aragon/dp/UsarEmpleado.java
@@ -0,0 +1,18 @@
+
+package mx.unam.aragon.dp;
+
+
+public class UsarEmpleado {
+    public static void main(String[] args) {
+        Empleado2 emp1 = new Empleado2(1, "Sistemas", 45000.0f, 15);  
+        emp1.setNombre("Jose");
+        emp1.setApPaterno("Garcia");
+        emp1.setApMaterno("Bolaños");
+        emp1.setEdad(25);
+        emp1.setCurp("GABJ121212HDFTR2");
+        
+        
+        System.out.println("info"+emp1);
+        
+    }   
+}
